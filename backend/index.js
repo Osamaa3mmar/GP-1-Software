dc40e3de1e94c27524ff664f2020ab3769fc3149
Osamaa3mmar <osamaammarothman@gmail.com>
2025-03-10 15:46:26 +0200

    init backend (Folder structure)

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from "cors";
+import { initApp } from './src/index.router.js';
+const app= express();
+const port=4545; 
+app.use(cors());
+app.use(express.json());
+initApp(app,express);
+
+
+
+app.listen(port ,()=>{
+    console.log("listening on port " + port);
+})
