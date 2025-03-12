7459615ce5a53b997561f0b7590be744b25e5d0e
Osamaa3mmar <osamaammarothman@gmail.com>
2025-03-12 17:49:34 +0200

    layout 2%

--- frontend/website/src/App.jsx
+++ frontend/website/src/App.jsx
@@ -1,8 +1,28 @@
-
+import {createBrowserRouter, Link, RouterProvider} from 'react-router-dom';
 export default function App() {
+  const router=createBrowserRouter([
+    {path:'/',
+      element:<div>Landing Page <Link to={'/auth/login'}>Login</Link> </div>
+    },
+    {path:"auth",
+      element:<div>Auth Layout </div>,
+      children:[
+        {
+          index:true,
+          element:<div>Login</div>,
+        },
+        {path:"login",
+          element:<div>Login</div>,
+        }, {path:"signup",
+          element:<div>signup</div>,
+        }
+
+      ]
+    }
+  ]);
   return (
     <div>
-      
+      <RouterProvider router={router}/>
     </div>
   )
 }
