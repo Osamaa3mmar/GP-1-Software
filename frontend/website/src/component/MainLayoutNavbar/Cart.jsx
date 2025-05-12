c1284ad626c0e10e8fba18d99c820cc4edca27f5
MusabAbuBshara <musababubshara@gmail.com>
2025-05-13 00:30:50 +0300

    cart button in main navbar

--- frontend/website/src/component/MainLayoutNavbar/Cart.jsx
+++ frontend/website/src/component/MainLayoutNavbar/Cart.jsx
@@ -2,13 +2,15 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import IconButton from '@mui/material/IconButton';
 import Badge from '@mui/material/Badge';
 import { useCart } from '../../contexts/CartContext';
+import { useNavigate } from 'react-router-dom';
 
 export default function Cart() {
   const { cartItems } = useCart();
-  const itemCount = cartItems.reduce((sum, item) => sum + item.quantity, 0);
+  const itemCount = cartItems.length;
+  const navigate = useNavigate();
 
   return (
-    <IconButton aria-label="cart">
+    <IconButton aria-label="cart" onClick={() => navigate('/main/cart')}>
       <Badge badgeContent={itemCount} color="primary">
         <ShoppingCartIcon sx={{ fontSize: "26px", color: "black" }}/>
       </Badge>
